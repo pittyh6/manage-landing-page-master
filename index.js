f54120e81d572723af4bf521b7dce87c843a1ebb
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-03-26 10:22:25 +1100

    [wdb-654][wdb-655][wdb-656] testeminail. Mobile, tablet and desktop version

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 $(function(){
     $(".toggle").on("click", function(){
         if($(".item").hasClass("active")){
@@ -10,3 +12,79 @@ $(function(){
         }
     });
 });
+
+const screenWidth = screen.width;
+console.log(screenWidth)
+
+/*carrossel*/
+    function moveCarrossel(){       
+        let time = 2000,
+        currentCommentMobile = 0,
+        currentCommentDesktop = 3,
+        comments = document.querySelectorAll(".comment"),
+        max = comments.length,
+        dotElement = document.querySelectorAll(".dot") 
+
+        // Mobile Verison
+        if(screenWidth <= 768){
+            setInterval(()=> {
+                comments[currentCommentMobile]
+                    .classList.remove("selected");
+
+                dotElement[currentCommentMobile]
+                    .classList.remove("dotClicked");
+                        
+                currentCommentMobile++
+                
+    
+                if(currentCommentMobile >= max){
+                    currentCommentMobile = 0
+                }
+        
+                comments[currentCommentMobile]
+                        .classList.add("selected")
+
+                dotElement[currentCommentMobile]
+                        .classList.add("dotClicked");
+                }, time) 
+        }else{//Desktop Version
+            comments[0].classList.add("selected");
+            comments[1].classList.add("selected");
+            comments[2].classList.add("selected");
+
+            setInterval(()=> {
+                comments[currentCommentMobile]
+                    .classList.remove("selected");
+    
+                    currentCommentMobile++
+    
+                if(currentCommentMobile >= max){
+                    currentCommentMobile = 0
+                }
+        
+                comments[currentCommentDesktop]
+                        .classList.add("selected")
+
+                        currentCommentDesktop++
+
+                if(currentCommentDesktop >= max){
+                    currentCommentDesktop = 0
+                } 
+            }, time)                       
+        }
+           
+    }
+
+    function startCarrossel(){
+        // setInterval(()=> {
+        //     nextComment()
+        // }, time)
+        moveCarrossel();
+    }   
+    function ready() {
+        startCarrossel()
+    }
+    document.addEventListener("DOMContentLoaded", ready);
+
+    //window.addEventListener("load", start)
+/**/
